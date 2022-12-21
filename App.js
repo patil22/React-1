ac4f97c4695ce2f0276a28ade33d6222e00f4ada
patil22 <113407263+patil22@users.noreply.github.com>
2022-12-21 06:02:00 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+import './othercss.css'
+import './index.css'
+import Sidebar  from './Components/Sidebar';
+import Navbar from './Components/Navbar';
+function App() {
+  return (
+    <>
+    <Navbar/>
+   {/* <Sidebar/>
+*/ }
+
+<button type = "button" className="btn btn-primary">ADD</button>
+
+    </>
+  );
+
+}
+
+export default App;
+
+
